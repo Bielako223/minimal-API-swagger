91a490382f1ecafd2ab4184a479a58c9551ae54e
Mateusz bielaszewski <mateusz-bielaszewski@o2.pl>
2023-04-23 15:24:06 +0200

    first commit

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace DataAccess.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionId = "Default")
+        {
+            using DbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+            var ret = await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+            return ret.ToList();
+        }
+
+        public async Task SaveData<T>(string storedProcedure, T parameters, string connectionId = "Default")
+        {
+            using DbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
+
+            await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,10 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+insert into dbo.[User] (FirstName, LastName)
+values ('Matełuszek','Ja'),
+('Jan','Pan'),
+('Michalek', 'Kwiatuszewski'),
+('Jarema','Fetorek');
+
+end
+
